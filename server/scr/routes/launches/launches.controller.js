f999b2116a12f14a0d722e2cc1211bc220c71f57
Akaniru Eunice <chieunice114@gmail.com>
2022-02-04 10:18:04 -0800

    post/launches: i created a validation for the post request

--- server/scr/routes/launches/launches.controller.js
+++ server/scr/routes/launches/launches.controller.js
@@ -6,8 +6,16 @@ function httpGetAllLaunches(req, res){
 function httpAddNewLaunch(req, res){
     const launch = req.body
 
-    launch.launchDate = new Date(launch.launchDate)
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+        error: "Missing required launch property"
+    }
 
+    launch.launchDate = new Date(launch.launchDate)
+    if(isNaN(launch.launchDate)){
+        return res.status(400).json({
+            error: "Invalid launch date"
+        })
+    }
     addNewLaunch(launch)
     return res.status(201).json(launch)
 }
