71e4aab9cf9fba22a9c862a766f61ea2c08ed37f
Akaniru Eunice <chieunice114@gmail.com>
2022-02-06 00:28:17 -0800

    testing APIS with jest

---
+++ server/scr/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe('Test GET /launches', () => {
+    test('It should respone with 200 success', () => {
+        const response = 200;
+        expect(response).toBe(200);
+    })
+}) 
+
+describe('Test POST /launches', () => {
+    test('It should respone with 200 succes', () => {
+
+    })
+    test('It should catch missing required properties', () => {});
+    test('It should catch invaild date', () => {})
+})
