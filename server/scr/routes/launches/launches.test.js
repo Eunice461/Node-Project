eb9b15c2bce90cdc9a3d838e736cd405f9047c73
Akaniru Eunice <chieunice114@gmail.com>
2022-02-06 01:14:42 -0800

    testing API endpoints with supertest: POST

--- server/scr/routes/launches/launches.test.js
+++ server/scr/routes/launches/launches.test.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const request = require('supertest')
 const app = require('../../app');
 
@@ -11,8 +12,31 @@ describe('Test GET /launches', () => {
 }) 
 
 describe('Test POST /launches', () => {
-    test('It should respone with 200 succes', () => {
+    const completeLaunchData = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC I701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2028',
+    }
 
+    const launchDateWithoutDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC I701-D',
+        target: 'Kepler-186 f',
+    }
+
+    test('It should respone with 201 created', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(completeLaunchData)
+        .expect('Content-Type', /json/)
+        .expect(201)
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date (response.body.launchDate).valueOf();
+        expect(responseDate).toBe(requestDate)
+        
+        expect(response.body).toMatchObject(launchDateWithoutDate)
     })
     test('It should catch missing required properties', () => {});
     test('It should catch invaild date', () => {})
