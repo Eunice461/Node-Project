6004ad06f30fdc57b4294a4649e1e1393a6d1368
Akaniru Eunice <chieunice114@gmail.com>
2022-02-14 23:55:23 -0800

    using passport to initialize our oauth authentication

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require('express')
 const helmet = require('helmet')
 const passport = require('passport')
 const { Strategy } = require('passport-google-oauth20')
+const cookieSession = require('cookie-session')
+const { verify } = require('crypto')
+const UserInfoError = require('passport-google-oauth20/lib/errors/userinfoerror')
 
 require('dotenv').config()
 
@@ -13,6 +16,8 @@ const PORT = 3000
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
 }
 
 const AUTH_OPTIONS = {
@@ -21,7 +26,7 @@ const AUTH_OPTIONS = {
     clientSecret: config.CLIENT_SECRET,
   };
   
-
+//use to get our profile from gooogle
 function verifyCallback(accessToken, refreshToken, profile, done) {
     console.log('Google profile', profile);
     done(null, profile);
@@ -29,13 +34,43 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
   
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
+//Serializeuser is when we save the session to the cookie
+passport.serializeUser((user, done) => {
+    //passing the id mean we want the session to send back only the id to avoid sending in more datas because our cookies can only save 4kb on the browesr
+    // we can either send the obj or the id
+    done(null, user.id)
+})
+
+//Deserializeuser is when we read the session from the cookie
+passport.deserializeUser((id, done) => {
+    done(null, id)
+})
+
+//FOR DATABASE
+// //looking for user.id provided in the serializeuser in the database using the id provide for goggle that is been save in our database
+// passport.deserializeUser((id, done) => {
+//     // the User is the name used to store our user schema in the database
+//     User.findById(id).then( user => {
+//         done(null, user)
+//     })
+// })
 const app = express();
 
 app.use(helmet());
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ]
+}))
+
 app.use(passport.initialize())
 
+// it keep track of our loginin users
+app.use(passport.session())
+
 function checkLoggedIn(req, res, next) {
-    const isLoggedIn = true; //TODO
+    const isLoggedIn = true;
     if(!isLoggedIn){
         return res.status(401).json({
             error: 'you must log in'
@@ -51,7 +86,7 @@ app.get('/auth/google', passport.authenticate('google', {
 app.get('/auth/google/callback', passport.authenticate('google', {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false,
+    session: true,
 }), 
 (req, res) => {
     console.log('Google called us back');
